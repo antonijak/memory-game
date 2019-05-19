b6bd9f0e7b10a2d04af69bf3440085c8f69a54c8
Antonija K <antonija.kasum@gmail.com>
2019-05-19 20:28:45 +0300

    open two cards

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ import umbrella from './assets/umbrella.jpg';
 class App extends Component {
   state = {
     cards: [ace, earphones, smiley, umbrella],
-    stack: []
+    stack: [],
+    open: {
+      first: { picture: '', card: '' },
+      second: { picture: '', card: '' }
+    }
   };
   createRandomStack = max => {
     // create array of random numbers starting from 0 to max
@@ -28,16 +32,60 @@ class App extends Component {
 
     this.setState({ stack: this.createRandomStack(8), cards });
   };
+  handleClick = (pictureId, cardId) => {
+    const first = this.state.open.first.picture;
+    const second = this.state.open.second.picture;
+    if (first !== '' && second !== '') {
+      this.setState({
+        open: {
+          first: { picture: '', card: '' },
+          second: { picture: '', card: '' }
+        }
+      });
+    } else if (
+      first !== '' &&
+      second === '' &&
+      cardId !== this.state.open.first.card
+    ) {
+      this.setState({
+        open: {
+          ...this.state.open,
+          second: { picture: pictureId, card: cardId }
+        }
+      });
+    } else {
+      this.setState({
+        open: {
+          ...this.state.open,
+          first: { picture: pictureId, card: cardId }
+        }
+      });
+    }
+  };
+  return0to3 = num => {
+    return num > 3 ? num - 4 : num;
+  };
   render() {
-    const { stack, cards } = this.state;
+    const { stack, cards, open } = this.state;
     return (
       <div className="App">
         <div className="game">
           {stack.map((card, i) => (
-            <div key={i + 1} className="game__card">
+            <div
+              key={i + 1}
+              className="game__card"
+              onClick={() => this.handleClick(this.return0to3(card), i + 1)}
+            >
               <img
-                className="game__card__image"
-                src={card > 3 ? cards[card - 4] : cards[card]}
+                className={
+                  (open.first.picture === this.return0to3(card) &&
+                    open.first.card === i + 1) ||
+                  (open.second.picture === this.return0to3(card) &&
+                    open.second.card === i + 1)
+                    ? 'game__card__image'
+                    : 'game__card__image game__card__image--hidden'
+                }
+                src={cards[this.return0to3(card)]}
                 alt={`card${card}`}
               />
             </div>
