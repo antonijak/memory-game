e11ebf55037b4366647febf4f0c5c237612766bb
Antonija K <antonija.kasum@gmail.com>
2019-05-19 21:30:32 +0300

    remove matched cards

--- src/App.js
+++ src/App.js
@@ -8,92 +8,109 @@ import smiley from './assets/smiley.jpg';
 import umbrella from './assets/umbrella.jpg';
 
 class App extends Component {
-  state = {
-    cards: [ace, earphones, smiley, umbrella],
-    stack: [],
-    open: {
-      first: { picture: '', card: '' },
-      second: { picture: '', card: '' }
-    }
-  };
-  createRandomStack = max => {
-    // create array of random numbers starting from 0 to max
-    let arr = [];
-    for (let i = 0; arr.length < max; i++) {
-      let num = Math.floor(Math.random() * max);
-      !arr.includes(num) && arr.push(num);
-    }
-    return arr;
-  };
-  componentDidMount = () => {
-    const deck = this.state.cards;
-    //duplicate all cards
-    const cards = deck.concat(deck);
+	state = {
+		cards: [ace, earphones, smiley, umbrella],
+		stack: [],
+		open: {
+			first: { picture: '', card: '' },
+			second: { picture: '', card: '' }
+		},
+		score: 0
+	};
 
-    this.setState({ stack: this.createRandomStack(8), cards });
-  };
-  handleClick = (pictureId, cardId) => {
-    const first = this.state.open.first.picture;
-    const second = this.state.open.second.picture;
-    if (first !== '' && second !== '') {
-      this.setState({
-        open: {
-          first: { picture: '', card: '' },
-          second: { picture: '', card: '' }
-        }
-      });
-    } else if (
-      first !== '' &&
-      second === '' &&
-      cardId !== this.state.open.first.card
-    ) {
-      this.setState({
-        open: {
-          ...this.state.open,
-          second: { picture: pictureId, card: cardId }
-        }
-      });
-    } else {
-      this.setState({
-        open: {
-          ...this.state.open,
-          first: { picture: pictureId, card: cardId }
-        }
-      });
-    }
-  };
-  return0to3 = num => {
-    return num > 3 ? num - 4 : num;
-  };
-  render() {
-    const { stack, cards, open } = this.state;
-    return (
-      <div className="App">
-        <div className="game">
-          {stack.map((card, i) => (
-            <div
-              key={i + 1}
-              className="game__card"
-              onClick={() => this.handleClick(this.return0to3(card), i + 1)}
-            >
-              <img
-                className={
-                  (open.first.picture === this.return0to3(card) &&
-                    open.first.card === i + 1) ||
-                  (open.second.picture === this.return0to3(card) &&
-                    open.second.card === i + 1)
-                    ? 'game__card__image'
-                    : 'game__card__image game__card__image--hidden'
-                }
-                src={cards[this.return0to3(card)]}
-                alt={`card${card}`}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }
+	createRandomStack = max => {
+		// create array of random numbers starting from 0 to max
+		let arr = [];
+		for (let i = 0; arr.length < max; i++) {
+			let num = Math.floor(Math.random() * max);
+			!arr.includes(num) && arr.push(num);
+		}
+		//return only numbers 0-3 in the array
+		return arr.map(num => (num > 3 ? num - 4 : num));
+	};
+
+	componentDidMount = () => {
+		//this code is for future when number and source of cards could be changed
+		const deck = this.state.cards;
+		//duplicate all cards
+		const cards = deck.concat(deck);
+
+		this.setState({ stack: this.createRandomStack(8), cards });
+	};
+
+	givePoint = picture => {
+		setTimeout(() => {
+			this.setState({
+				stack: this.state.stack.filter(item => item !== picture),
+				score: this.state.score + 1
+			});
+		}, 500);
+	};
+
+	handleClick = (picture, card) => {
+		const { picture: firstPic, card: firstCard } = this.state.open.first;
+		const secondPic = this.state.open.second.picture;
+
+		//check if any cards are already selected and populate "open": first "first" then "second"
+		if (firstPic !== '' && secondPic !== '') {
+			this.setState({
+				open: {
+					first: { picture: '', card: '' },
+					second: { picture: '', card: '' }
+				}
+			});
+		} else if (firstPic !== '' && secondPic === '' && card !== firstCard) {
+			this.setState({
+				open: {
+					...this.state.open,
+					second: { picture, card }
+				}
+			});
+
+			picture === firstPic && this.givePoint(picture);
+		} else {
+			this.setState({
+				open: {
+					...this.state.open,
+					first: { picture, card }
+				}
+			});
+		}
+	};
+
+	render() {
+		const { stack, cards, open } = this.state;
+		const { picture: firstPicture, card: firstCard } = open.first;
+		const { picture: secondPicture, card: secondCard } = open.second;
+
+		return (
+			<div className="App">
+				<div className="game">
+					{stack.map((picture, i) => {
+						const card = i + 1;
+						return (
+							<div
+								key={card}
+								className="game__card"
+								onClick={() => this.handleClick(picture, card)}
+							>
+								<img
+									className={
+										(firstPicture === picture && firstCard === card) ||
+										(secondPicture === picture && secondCard === card)
+											? 'game__card__image'
+											: 'game__card__image game__card__image--hidden'
+									}
+									src={cards[picture]}
+									alt={`card${picture}`}
+								/>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
