04ed6f9ccb311a344d4a2f4711a93ea466f7c2a3
Antonija K <antonija.kasum@gmail.com>
2019-05-19 18:51:32 +0300

    display cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//images
+import ace from './assets/ace.jpg';
+import earphones from './assets/earphones.jpg';
+import smiley from './assets/smiley.jpg';
+import umbrella from './assets/umbrella.jpg';
+
+class App extends Component {
+  state = {
+    cards: [ace, earphones, smiley, umbrella],
+    stack: []
+  };
+  createRandomStack = max => {
+    // create array of random numbers starting from 0 to max
+    let arr = [];
+    for (let i = 0; arr.length < max; i++) {
+      let num = Math.floor(Math.random() * max);
+      !arr.includes(num) && arr.push(num);
+    }
+    return arr;
+  };
+  componentDidMount = () => {
+    const deck = this.state.cards;
+    //duplicate all cards
+    const cards = deck.concat(deck);
+
+    this.setState({ stack: this.createRandomStack(8), cards });
+  };
+  render() {
+    const { stack, cards } = this.state;
+    return (
+      <div className="App">
+        <div className="game">
+          {stack.map((card, i) => (
+            <div key={i + 1} className="game__card">
+              <img
+                className="game__card__image"
+                src={card > 3 ? cards[card - 4] : cards[card]}
+                alt={`card${card}`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
